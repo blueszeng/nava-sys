100d00764c112f22c7ad55c4253ab5819f3bdfa0
Kasem Anontavilas <mrtomyum@gmail.com>
2016-07-15 12:30:25 +0700

    Change DB refierence by Dependency Injection

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mrtomyum/nava-api3/models"
-	"net/http"
-	"fmt"
 	"log"
-	"encoding/json"
+	"net/http"
+	"github.com/mrtomyum/nava-api3/controllers"
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -19,32 +19,32 @@ const (
 var dsn = DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME + "?charset=utf8"
 
 func main() {
-	db := models.InitDB(dsn)
+	db, err := models.NewDB(dsn)
+	if err != nil {
+		log.Panic("NewDB() Error:", err)
+	}
+
+	c := &controllers.Env{DB: db}
+
 	defer db.Close()
 	log.Println("start InitDB()")
-	http.HandleFunc("/", mainIndex)
-	log.Println("start HandleFunc('/')")
-	http.HandleFunc("/users", userIndex)
-	log.Println("start HandleFunc('/users')")
+
+	r := mux.NewRouter()
+	http.HandleFunc("/api", controllers.MainIndex)
+	log.Println("start HandleFunc('/api')")
+	http.HandleFunc("/api/v1/test", controllers.TestIndex)
+	log.Println("start HandleFunc('/api/v1/test')")
+
+
+	//http.HandleFunc("/api/v1/users", c.UserIndex)
+	//log.Println("start HandleFunc('/api/v1/users')")
+
+	r.HandleFunc("/api/v1/users", c.UserIndex).Methods("GET")
+	log.Println("start HandleFunc('/api/v1/users') GET")
+	r.HandleFunc("/api/v1/users", c.UserAdd).Methods("POST")
+	log.Println("start HandleFunc('/api/v1/users') POST")
+	r.HandleFunc("/api/v1/users", c.UserEdit).Methods("PUT")
+	log.Println("start HandleFunc('/api/v1/users') PUT")
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
-
-}
-
-func mainIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v", "Hello")
-}
-
-func userIndex(w http.ResponseWriter, r *http.Request) {
-	log.Println("start userIndex()")
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-	users, err := models.AllUsers()
-	if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-	o, _ := json.Marshal(users)
-	fmt.Fprintf(w, string(o))
 }

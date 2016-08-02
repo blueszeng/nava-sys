8ec0d796bebb61ce51e1a794b49310f6e7609627
Kasem Arnontavilas <mrtomyum@gmail.com>
2016-08-02 19:19:39 +0700

    Refactor Function CreateTree() from controller.menu

--- controller/menu.go
+++ controller/menu.go
@@ -42,7 +42,6 @@ func (e *Env) MenuInsert(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("Error Insert DB:", err)
 		w.WriteHeader(http.StatusNotImplemented)
-
 	}
 	w.WriteHeader(http.StatusOK)
 	output, _ := json.Marshal(&m)
@@ -56,39 +55,15 @@ func (e *Env) MenuTree(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*") //to allow cross domain AJAX.
+
 	menu := new(m.Menu)
 	menus, err := menu.All(e.DB)
 	if err != nil {
 		log.Println("Error in m.Menu.All: ", err)
 	}
-	// Setup root node
-	jsonNode := new(m.Node)
-	jsonNode.ID = menus[0].ID
-	jsonNode.ParentID = menus[0].ParentID
-	jsonNode.Text = menus[0].Text
-	jsonNode.Icon = menus[0].Icon
-	jsonNode.SelectedIcon = menus[0].SelectedIcon
-	jsonNode.Href = menus[0].Href
-	jsonNode.Path = menus[0].Path
-	jsonNode.Note = menus[0].Note
-	// Adding child node
-	for k, menu := range menus{
-		n := new(m.Node)
-		if k != 0 {
-			n.ID = menu.ID
-			n.ParentID = menu.ParentID
-			n.Text = menu.Text
-			n.Icon = menu.Icon
-			n.SelectedIcon = menu.SelectedIcon
-			n.Href = menu.Href
-			n.Path = menu.Path
-			n.Note = menu.Note
-			log.Println("n=", n)
-			jsonNode.Add(n)
-		}
-	}
+	tree := CreateTree(menus)
 	w.WriteHeader(http.StatusOK)
-	output, _ := json.Marshal(jsonNode.Child) // remove root node from jsonNode
+	output, _ := json.Marshal(tree.Child) // remove root node from jsonNode
 	fmt.Fprintf(w, string(output))
 }
 
